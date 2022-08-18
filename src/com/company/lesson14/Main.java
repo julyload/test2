5dbb0300d62f11d11d88e027125f5a74bf966744
julyload <thindotted@gmail.com>
2022-08-18 12:36:04 +0300

    Init

--- src/com/company/lesson14/Main.java
+++ src/com/company/lesson14/Main.java
@@ -2,7 +2,7 @@ package com.company.lesson14;
 
 public class Main {
     public static void main(String[] args)  {
-        User user = new User();
+        User user = new User("Pavel", 12);
         System.out.println(user.toString());
         User user2 = new User();
         System.out.println(user2.toString());
